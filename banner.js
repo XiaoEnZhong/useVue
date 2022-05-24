cbf7674ab2a00461c3b64db1d3c02e0e91ffd1bd
XiaoEnZhong <62697271+XiaoEnZhong@users.noreply.github.com>
2022-05-24 18:01:26 +0800

    v
    v

---
+++ banner.js
@@ -0,0 +1,122 @@
+let vm = new Vue({
+    el: '#mainbanner',
+    template: `
+
+    <div class="slick">
+	<div class="caption"
+				style="background-image: url(//landing.mangot5.com//Images/cms/happycode/20191209/1575889989121.jpg);">
+				<div class="container">
+					<div class="wrapper">
+						<p><p>《十二之天貳》</p></p>
+						<a href="https://12sky2.mangot5.com/" target="_blank">了解更多</a>
+					</div>
+				</div>
+			</div>
+		<div class="caption"
+				style="background-image: url(//landing.mangot5.com//Images/cms/happycode/20191209/1575889464058.jpg);">
+				<div class="container">
+					<div class="wrapper">
+						<p><p>《封印者》</p></p>
+						<a href="https://landing.mangot5.com/template/closers/event/190516_update/index.html" target="_blank">了解更多</a>
+					</div>
+				</div>
+			</div>
+		<div class="caption"
+				style="background-image: url(//landing.mangot5.com//Images/cms/happycode/20191209/1575889237684.jpg);">
+				<div class="container">
+					<div class="wrapper">
+						<p><p>《ROH》</p></p>
+						<a href="https://roh.mangot5.com/" target="_blank">了解更多</a>
+					</div>
+				</div>
+			</div>
+		<div class="caption"
+				style="background-image: url(//landing.mangot5.com//Images/cms/happycode/20191209/1575889196146.jpg);">
+				<div class="container">
+					<div class="wrapper">
+						<p><p>《TERA》</p></p>
+						<a href="https://tera.mangot5.com/tera/index" target="_blank">了解更多</a>
+					</div>
+				</div>
+			</div>
+		<div class="caption"
+				style="background-image: url(//landing.mangot5.com//Images/cms/happycode/20191209/1575889152363.jpg);">
+				<div class="container">
+					<div class="wrapper">
+						<p><p>《漢堡物語》</p></p>
+						<a href="https://burger.mangot5.com/burger/index.html" target="_blank">了解更多</a>
+					</div>
+				</div>
+			</div>
+		<div class="caption"
+				style="background-image: url(//landing.mangot5.com//Images/cms/happycode/20191209/1575889121346.jpg);">
+				<div class="container">
+					<div class="wrapper">
+						<p><p>《新熱血江湖》</p></p>
+						<a href="http://landing.mangot5.com/template/hot/event/190516/#page_1" target="_blank">了解更多</a>
+					</div>
+				</div>
+			</div>
+		<div class="caption"
+				style="background-image: url(//landing.mangot5.com//Images/cms/happycode/20191209/1575889084785.jpg);">
+				<div class="container">
+					<div class="wrapper">
+						<p><p>《AIKA》</p></p>
+						<a href="https://aika.mangot5.com/" target="_blank">了解更多</a>
+					</div>
+				</div>
+			</div>
+		<div class="caption"
+				style="background-image: url(//landing.mangot5.com//Images/cms/happycode/20191209/1575889043038.jpg);">
+				<div class="container">
+					<div class="wrapper">
+						<p><p>《SF2》</p></p>
+						<a href="https://landing.mangot5.com/template/sf2/event/190813_catastrophe/index.html" target="_blank">了解更多</a>
+					</div>
+				</div>
+			</div>
+		<div class="caption"
+				style="background-image: url(//landing.mangot5.com//Images/cms/happycode/20191209/1575888980287.jpg);">
+				<div class="container">
+					<div class="wrapper">
+						<p><p>《勁舞團 M》</p></p>
+						<a href="https://aum.mangot5.com/aum/index" target="_blank">了解更多</a>
+					</div>
+				</div>
+			</div>
+		<div class="caption"
+				style="background-image: url(//landing.mangot5.com//Images/cms/happycode/20191209/1575888901368.png);">
+				<div class="container">
+					<div class="wrapper">
+						<p><p>《勁舞團》</p></p>
+						<a href="https://au.mangot5.com/" target="_blank">了解更多</a>
+					</div>
+				</div>
+			</div>
+		<div class="caption"
+				style="background-image: url(//landing.mangot5.com//Images/cms/happycode/20190905/1567651559383.png);">
+				<div class="container">
+					<div class="wrapper">
+						<p><p>《疾風之刃 Online》</p></p>
+						<a href="https://landing.mangot5.com/template/kritika/event/190905_red/index.html" target="_blank">了解更多</a>
+					</div>
+				</div>
+			</div>
+		<div class="caption"
+				style="background-image: url(//landing.mangot5.com//Images/cms/happycode/20191209/1575888710480.jpg);">
+				<div class="container">
+					<div class="wrapper">
+						<p><p>《彩虹島物語》</p></p>
+						<a href="https://la.mangot5.com/" target="_blank">了解更多</a>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+
+
+
+
+`
+})
---
+++ news.js
@@ -0,0 +1,111 @@
+let vms = new Vue({
+    el: '#notice',
+    template: `
+
+    <div class="notice">
+	      	<ul>
+	      	<li>
+	      			<span class="num hidden-xs"><a href="javascript:void(0);">•</a></span>
+	      			<span class="info">
+	      				<a href="https://company.mangot5.com//company/information/49428">
+	      					<span class="cat hidden-xs">[全新地圖]</span>
+	      						《A.V.A 戰地之王》全新爆破戰場「鐵鏽軍工廠」好評來襲</a>
+	      			</span>
+	      			<span class="time">
+	      				2022.05.16</span>
+	      		</li>
+	      	<li>
+	      			<span class="num hidden-xs"><a href="javascript:void(0);">•</a></span>
+	      			<span class="info">
+	      				<a href="https://company.mangot5.com//company/information/49385">
+	      					<span class="cat hidden-xs">[改版]</span>
+	      						《封印者：CLOSERS》」新副本「海上橋梁守護作戰」 復刻時裝「女僕&執事」和「東方亡者」</a>
+	      			</span>
+	      			<span class="time">
+	      				2022.05.12</span>
+	      		</li>
+	      	<li>
+	      			<span class="num hidden-xs"><a href="javascript:void(0);">•</a></span>
+	      			<span class="info">
+	      				<a href="https://company.mangot5.com//company/information/49382">
+	      					<span class="cat hidden-xs">[賽事新聞]</span>
+	      						《封印者：CLOSERS》第九屆PVP鬪神演武暨五周年同樂會 「諸神黃昏」總決賽結果揭曉</a>
+	      			</span>
+	      			<span class="time">
+	      				2022.05.12</span>
+	      		</li>
+	      	<li>
+	      			<span class="num hidden-xs"><a href="javascript:void(0);">•</a></span>
+	      			<span class="info">
+	      				<a href="https://company.mangot5.com//company/information/49380">
+	      					<span class="cat hidden-xs">[改版]</span>
+	      						宿命再臨 《風色幻想：覺醒Online》新職業、最強裝備同步公開</a>
+	      			</span>
+	      			<span class="time">
+	      				2022.05.12</span>
+	      		</li>
+	      	<li>
+	      			<span class="num hidden-xs"><a href="javascript:void(0);">•</a></span>
+	      			<span class="info">
+	      				<a href="https://company.mangot5.com//company/information/49188">
+	      					<span class="cat hidden-xs">[新系統上線]</span>
+	      						《封印者：CLOSERS》」新副本「拜見軍團長：機械王貝爾菲格勒」登場 新時裝「愛的誓言」同步上市</a>
+	      			</span>
+	      			<span class="time">
+	      				2022.04.28</span>
+	      		</li>
+	      	<li>
+	      			<span class="num hidden-xs"><a href="javascript:void(0);">•</a></span>
+	      			<span class="info">
+	      				<a href="https://company.mangot5.com//company/information/49061">
+	      					<span class="cat hidden-xs">[新系統上線]</span>
+	      						《A.V.A 戰地之王》新模式「奔跑吧！菁英！」上線 玻璃橋上的生存試煉</a>
+	      			</span>
+	      			<span class="time">
+	      				2022.04.18</span>
+	      		</li>
+	      	<li>
+	      			<span class="num hidden-xs"><a href="javascript:void(0);">•</a></span>
+	      			<span class="info">
+	      				<a href="https://company.mangot5.com//company/information/49054">
+	      					<span class="cat hidden-xs">[改版]</span>
+	      						《彩虹島物語》推出「克諾索斯」改版 踏上獸人帝國尋找止戰之心</a>
+	      			</span>
+	      			<span class="time">
+	      				2022.04.15</span>
+	      		</li>
+	      	<li>
+	      			<span class="num hidden-xs"><a href="javascript:void(0);">•</a></span>
+	      			<span class="info">
+	      				<a href="https://company.mangot5.com//company/information/48828">
+	      					<span class="cat hidden-xs">[賽事新聞]</span>
+	      						《Special Force 2》2022 春季王者爭霸戰圓滿結束 由「翔哥不要」奪冠</a>
+	      			</span>
+	      			<span class="time">
+	      				2022.03.30</span>
+	      		</li>
+	      	<li>
+	      			<span class="num hidden-xs"><a href="javascript:void(0);">•</a></span>
+	      			<span class="info">
+	      				<a href="https://company.mangot5.com//company/information/48753">
+	      					<span class="cat hidden-xs">[改版]</span>
+	      						《Special Force 2》2022 春季王者爭霸戰 3 月 26日 正式開打</a>
+	      			</span>
+	      			<span class="time">
+	      				2022.03.23</span>
+	      		</li>
+	      	<li>
+	      			<span class="num hidden-xs"><a href="javascript:void(0);">•</a></span>
+	      			<span class="info">
+	      				<a href="https://company.mangot5.com//company/information/48752">
+	      					<span class="cat hidden-xs">[新上市]</span>
+	      						萌系休閒放置手機遊戲《蒼穹英雄》事前預約火熱開跑</a>
+	      			</span>
+	      			<span class="time">
+	      				2022.03.23</span>
+	      		</li>
+	      	</ul>
+    </div>
+
+    `
+})
